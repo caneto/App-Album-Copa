d5238ab87081bef8c9d66e013382f9cd42c9d084
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-10-11 19:11:04 -0300

    Atualização

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:appalbumcopa/app/core/ui/helpers/loader.dart';
+import 'package:appalbumcopa/app/core/ui/helpers/messages.dart';
 import 'package:appalbumcopa/app/core/ui/styles/button_styles.dart';
 import 'package:appalbumcopa/app/core/ui/styles/text_styles.dart';
 import 'package:appalbumcopa/app/core/ui/widgets/button.dart';
@@ -12,41 +13,14 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> with Loader {
+class _SplashPageState extends State<SplashPage> with Loader<SplashPage>, Messages<SplashPage> {
   @override
   Widget build(BuildContext context) {
    return Scaffold(
      appBar: AppBar(
        title: const Text('Splash Screen'),
      ),
-     body: Center(
-       child: Column(
-         children: [
-           Button(
-             onPressed: () async {
-               showLoader();
-
-               await Future.delayed(Duration(seconds: 2));
-               hideLoader();
-             },
-             style: ButtonStyles.instance.yellowButton,
-             labelStyle: TextStyles.instance.textPrimaryFontBold,
-             label: 'Salvar',
-           ),
-           Button.primary(
-             onPressed: () {},
-             label: 'Salvar',
-           ),
-           Button.primary(
-             widht: MediaQuery.of(context).size.width * .9,
-             height: 30,
-             onPressed: () {},
-             label: 'Salvar',
-           ),
-           RoundedButton(icon: Icons.add, onPressed: () {})
-         ],
-       ),
-     ),
+     body: Container(),
    );
   }
 }
