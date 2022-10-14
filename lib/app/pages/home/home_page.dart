2a7faaef7f3e5f1bf09d8574e45b819c5ff7acb8
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-10-14 10:51:53 -0300

    Atualização da Home

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:appalbumcopa/app/core/ui/styles/button_styles.dart';
 import 'package:appalbumcopa/app/core/ui/styles/colors_app.dart';
+import 'package:appalbumcopa/app/core/ui/styles/text_styles.dart';
+import 'package:appalbumcopa/app/core/ui/widgets/button.dart';
+import 'package:appalbumcopa/app/pages/home/widgets/status_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,9 +15,90 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: context.colors.primary,
-        title: const Text('Home'),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.logout,
+                color: Colors.white,
+              )),
+        ],
       ),
-      body: Container(),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover
+          )
+        ),
+        child: LayoutBuilder(
+            builder: (_, constrains) { 
+                return ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constrains.maxHeight),
+                  child: Center(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.only(bottom: 35),
+                            child: Image.asset(
+                              'assets/images/bola.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const Text('Percentual !!!!'),
+                          const SizedBox(
+                             height: 20,
+                          ),
+                          Text(
+                            '45 Figurinhas',
+                            style: context.textStyles.titleWhite,
+                          ),
+                          const SizedBox(
+                             height: 20,
+                          ),
+                          StatusTile(
+                            icon: Image.asset('assets/images/all_icon.png'),
+                            label: 'Todas',
+                            value: 34,
+                          ),
+                          const SizedBox(
+                             height: 20,
+                          ),
+                          StatusTile(
+                            icon: Image.asset('assets/images/missing_icon.png'),
+                            label: 'Faltando',
+                            value: 567,
+                          ),
+                          const SizedBox(
+                             height: 20,
+                          ),
+                          StatusTile(
+                            icon: Image.asset('assets/images/repeated_icon.png'),
+                            label: 'Repetidas',
+                            value: 30,
+                          ),
+                          const SizedBox(
+                             height: 20,
+                          ),
+                        Button(
+                          onPressed: () {},
+                          widht: MediaQuery.of(context).size.width * .9,
+                          outline: true,
+                          style: context.buttonStyles.yellowOutlineButton,
+                          labelStyle: context.textStyles.textSecundaryFontExtraBold.copyWith(
+                            color: context.colors.yellow
+                          ),
+                          label: 'Minhas Figurinhas',
+                        ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+            },
+          )
+      ),
     );
   }
 }
