8e416e8edaabf5953eeb5b56b3840b2cb4f359c1
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-10-19 17:24:13 -0300

    Update sticker_detail_view_impl.dart

--- lib/app/pages/sticker_detail/view/sticker_detail_view_impl.dart
+++ lib/app/pages/sticker_detail/view/sticker_detail_view_impl.dart
@@ -6,8 +6,9 @@ import 'package:appalbumcopa/app/core/ui/helpers/messages.dart';
 import '../sticker_detail_page.dart';
 import './sticker_detail_view.dart';
 
-abstract class StickerDetailViewImpl extends State<StickerDetailPage> with Messages<StickerDetailPage>,Loader<StickerDetailPage> implements StickerDetailView {
-
+abstract class StickerDetailViewImpl extends State<StickerDetailPage>
+    with Messages<StickerDetailPage>, Loader<StickerDetailPage>
+    implements StickerDetailView {
   bool hasSticker = false;
   String countryCode = '';
   String stickerNumber = '';
@@ -17,11 +18,40 @@ abstract class StickerDetailViewImpl extends State<StickerDetailPage> with Messa
   @override
   void initState() {
     widget.presenter.view = this;
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      showLoader();
+      var arguments = ModalRoute.of(context)?.settings.arguments;
+
+      if(arguments != null) {
+        widget.presenter.load(
+          countryCode: countryCode,
+          stickerNumber: stickerNumber,
+          countryName: countryName,
+        );
+      } else {
+        Navigator.of(context).pop();
+        showError('Não foi possivel carregar a figurinha');
+      }
+      
+    });
     super.initState();
   }
 
   @override
-  void screenLoaded(bool hasSticker, String countryCode, String stickerNumber, String countryName, int amount) {
-    // TODO: implement screenLoaded
-  } 
+  void screenLoaded(
+    bool hasSticker,
+    String countryCode,
+    String stickerNumber,
+    String countryName,
+    int amount,
+  ) {
+    hideLoader();
+    setState(() {
+      this.hasSticker = hasSticker;
+      this.countryCode = countryCode;
+      this.stickerNumber = stickerNumber;
+      this.countryName = countryName;
+      this.amount = amount;
+    });
+  }
 }
