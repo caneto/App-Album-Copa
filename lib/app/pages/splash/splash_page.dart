8ea35a9ae9d9c687d89c577b87d9a2a2b798e5a4
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-12-10 17:26:21 -0300

    Update splash_page.dart

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,12 +1,8 @@
-import 'package:appalbumcopa/app/core/ui/helpers/loader.dart';
-import 'package:appalbumcopa/app/core/ui/helpers/messages.dart';
 import 'package:appalbumcopa/app/core/ui/styles/button_styles.dart';
 import 'package:appalbumcopa/app/core/ui/styles/colors_app.dart';
 import 'package:appalbumcopa/app/core/ui/styles/text_styles.dart';
 import 'package:appalbumcopa/app/core/ui/widgets/button.dart';
-import 'package:appalbumcopa/app/core/ui/widgets/rounded_button.dart';
 import 'package:appalbumcopa/app/pages/splash/presenter/splash_presenter.dart';
-import 'package:appalbumcopa/app/pages/splash/view/splash_view.dart';
 import 'package:appalbumcopa/app/pages/splash/view/splash_view_impl.dart';
 import 'package:flutter/material.dart';
 
